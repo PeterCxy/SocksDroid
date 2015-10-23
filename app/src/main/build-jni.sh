24b6e022ab74b2e4daca17adabf790013dd1bb43
Peter Cai <xqsx43cxy@126.com>
2015-10-23 22:44:24 +0800

    update build script
    Signed-off-by: Peter Cai <xqsx43cxy@126.com>

--- app/src/main/build-jni.sh
+++ app/src/main/build-jni.sh
@@ -6,6 +6,6 @@ rm -rf ../../libs
 ndk-build
 
 mkdir -p assets/armeabi-v7a
-cp libs/armeabi-v7a/tun2socks assets/armeabi-v7a/tun2socks
-rm -rf libs/*/tun2socks
+cp libs/armeabi-v7a/{tun2socks,pdnsd} assets/armeabi-v7a/
+rm -rf libs/*/{tun2socks,pdnsd}
 mv libs ../../
